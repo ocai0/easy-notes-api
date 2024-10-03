2f07fe2f6045fb491096e2810d729593a1b257b7
ocai0 <caioalvesfernandes@gmail.com>
2024-10-02 23:54:28 -0300

    Added svelte and tailwind to the project

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "../app.css";
+  let notes = [];
+  const getNotes = () => {
+    return [
+      {
+        data: "Teste",
+        theme: "yellow",
+        type: 1
+      },
+    ]
+  }
+  notes = getNotes()
+</script>
+
+<main class="w-screen h-screen p-3 text-slate-200 bg-slate-900">
+  {#each notes as {theme, data, type}}
+    <div class={`note ${theme}`}>
+      {data}
+    </div>
+  {/each}
+</main>
